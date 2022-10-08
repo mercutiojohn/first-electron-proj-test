72b8473891f3720f73d603f60a2a7afcecb81fea
mercutio <mercutio_john@gmail.com>
2022-10-08 09:18:20 +0800

    123

---
+++ src/main/index.dev.js
@@ -0,0 +1,29 @@
+/**
+ * This file is used specifically and only for development. It installs
+ * `electron-debug` & `vue-devtools`. There shouldn't be any need to
+ *  modify this file, but it can be used to extend your development
+ *  environment.
+ */
+
+/* eslint-disable */
+
+// Install `electron-debug` with `devtron`
+require('electron-debug')({ showDevTools: true })
+// import { BrowserWindow } from 'electron';
+// Install `vue-devtools`
+require('electron').app.on('ready', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    require('vue-devtools').install()
+  }
+  // let installExtension = require('electron-devtools-installer')
+  // installExtension.default(installExtension.VUEJS_DEVTOOLS)
+  //   .then(() => {})
+  //   .catch(err => {
+  //     console.log('Unable to install `vue-devtools`: \n', err)
+  //   })
+  //参考 https://www.cnblogs.com/wozho/p/10782654.html 和 https://github.com/SimulatedGREG/electron-vue/issues/242
+  // BrowserWindow.addDevToolsExtension('node_modules/vue-devtools/vender')  //手动加载vue-devtools，前提是 npm install vue-devtools --save-dev
+})
+
+// Require `main` process to boot app
+require('./index')
---
+++ src/renderer/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <div class="head-bar">
+      <div class="drag"></div>
+      <div class="func">
+        <div class="window-btn minimize" @click="minimizeMin"></div>
+        <div class="window-btn maximize" @click="maximizeMin"></div>
+        <div class="window-btn close" @click="closeMin"></div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { ipcRenderer } from "electron"
+  export default {
+    name: 'first-electron-proj',
+    methods:{
+      minimizeMin(){
+        console.log("minimize");
+        ipcRenderer.send('window-min')
+      },
+      maximizeMin(){
+        console.log("maximize");
+        ipcRenderer.send('window-max')
+      },
+      closeMin(){
+        console.log("close");
+        ipcRenderer.send('window-close')
+      },
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+  .head-bar{
+    height:33px;
+    /* background: #fff; */
+    width: 100%;
+    display: flex;
+    position: fixed;
+    top:0;
+    left: 0;
+  }
+  .head-bar .drag{
+    width: calc(100vw - 130px);
+    -webkit-app-region:drag;
+    -webkit-user-select:none;
+  }
+  .head-bar .func{
+    width: 130px;
+    display: flex;
+  }
+  .head-bar .func .window-btn{
+    /* background: #000; */
+    border: 1px solid #000;
+    width: calc(130px / 3);
+    height: 100%;
+    cursor: pointer;
+    transition: all .2s ease;
+  }
+  .head-bar .func .window-btn:hover{
+    background: #cecece;
+  }
+  .head-bar .func .window-btn:active{
+    background: #646464;
+  }
+  
+</style>
